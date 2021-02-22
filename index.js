6636b24a8277af013d15c8ec3fe72b2f4b57f2bf
Jacob Lepler <lepler.jacob@gmail.com>
2021-02-22 15:41:01 -0500

    commit tree 3aff13ba491d2a960fef1c37856eb277eb4bac0b

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Add your functions here
+// function map(srcArray){
+//     return srcArray.map(num => {
+//         -num;
+//     })
+// }
+
+function map(array, func) {
+    let r  = [];
+
+    array.forEach(el => {
+        r.push(func(el))
+    })
+    return r
+}
+
+// const numbers = [1,-1,2,3];
+
+// let sum = 0;
+// for (let n of numbers)
+//     sum += n;
+
+//     console.log(sum);
+
+// numbers.reduce((acc, curr) => {
+//     return acc + curr;
+// }, 0);
+
+function reduce(sourceObj, callback, startWithThisValue){
+    // initialize two variables to store values. 'initialValue' will be either set to what is given ('startWithThisValue'), or if not given, will start at the first index value
+    let initialValue = !!startWithThisValue ? startWithThisValue : sourceObj[0];
+    let initialIndex = !!startWithThisValue ? 0 : 1;
+
+    for (; initialIndex < sourceObj.length; initialIndex++) {
+        initialValue = callback(sourceObj[initialIndex], initialValue)
+    }
+
+    return initialValue;
+}
+
